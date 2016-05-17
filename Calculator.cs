511e1a808be7e96e8e65cb21568f99769d5d4d35
tvidal02 <thiago_acvidal@hotmail.com>
2016-05-16 23:59:44 -0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class Calculator
+    {
+        //Em toda classe estática, os atributos têm de ser estáticos
+        public static int firstOperand;
+        public static int secondOperand;
+
+        //Em toda classe estática, os métodos têm de ser estáticos
+        public static int Calculate(int a, int b, Operator op)
+        {
+            if (op.Equals(Operator.PlusOp)) return a + b;
+            if (op.Equals(Operator.MinusOp)) return a - b;
+            if (op.Equals(Operator.MultOp)) return a * b;
+            if (op.Equals(Operator.DivOp)) return a / b;
+
+            return 0;
+        }
+
+        public static int FirstOperand
+        {
+            get { return firstOperand; }
+            set { firstOperand = value; }
+        }
+
+        public static int SecondOperand
+        {
+            get { return secondOperand; }
+            set { secondOperand = value; }
+        }
+
+        //Enum, assim como destrutores e indexadores, não aceita o modificador static
+        public enum Operator
+        {
+            MinusOp = '-',
+            PlusOp = '+',
+            MultOp = '*',
+            DivOp = '/'
+        }
+
+        public static Operator GetRandomOperator() 
+        {
+            var listOp = new List<Operator>() 
+            {
+                Operator.DivOp,
+                Operator.MinusOp,
+                Operator.MultOp,
+                Operator.PlusOp
+            };
+
+            Random r = new Random();
+            //seleciona aleatoriamente o índice do operador da lista de operadores
+            //por exemplo, o operador "Minus" = 1
+            int i = r.Next(listOp.Count);
+            var op = listOp[i]; 
+
+            return op;
+        }
+
+        static Calculator()
+        {
+            FirstOperand = new Random().Next(45);
+            SecondOperand = new Random().Next(100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //class Test {
+        //    static int n1 = 0;
+
+        //    public static void Y() 
+        //    {
+        //        this.n1 = 1;
+        //    }
+
+        //    //public void X() 
+        //    //{
+        //    //    this.n1 = 1;
+        //    //}
+        //}
+
+        static void Main(string[] args)
+        {
+            var x = new Test();
+            
+
+            //Propriedades estáticas também são acessadas pelo nome da classe
+            int num1 = Calculator.FirstOperand;
+            int num2 = Calculator.SecondOperand;
+            var opr = Calculator.GetRandomOperator();
+
+            int res = Calculator.Calculate(num1, num2, opr);
+            Console.WriteLine("Resultado: " + res);
+            
+            Console.ReadKey();
+        }
+    }
+}
